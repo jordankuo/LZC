a02da6fa2a3ee6bdafe6366dcf230b373877f41d
jordan_kuo <s104062638@m104.nthu.edu.tw>
2015-11-16 21:50:33 +0800

    add new lzc_t.v

--- lzc_t.v
+++ lzc_t.v
@@ -28,24 +28,41 @@ module stimulus;
 		#(cyc) rst_n = 1;
 		#(cyc) mode = 0;
 		
-		#(cyc) data = 8'b0000_0000;
+		//contiuous input
+		#(cyc) data = 8'b0000_0001;
 		#(cyc) Ivalid = 1;
 		#(cyc*4) Ivalid = 0;
 		
-		#(cyc) data = 8'b0011_1111;
-		#(cyc*2) Ivalid = 1;
+		//discontiuous input
+		#(cyc) data = 8'b0011_1111;Ivalid = 1;
 		#(cyc) Ivalid = 0;
 		#(cyc) Ivalid = 1;
 		#(cyc) Ivalid = 0;
 		#(cyc) Ivalid = 1;
+		#(cyc) Ivalid = 0;
+		#(cyc) data = 8'b0000_0000; Ivalid = 1; 
 		#(cyc) Ivalid = 0;
 		
-		#(cyc) data = 8'b0000_0000; Ivalid = 1;
-		#(cyc) data = 8'b0001_0000;
+		//contiuous diff. input
+		#(cyc) data = 8'b0001_0000;Ivalid = 1;
 		#(cyc) data = 8'b0011_1111;
 		#(cyc) data = 8'b1111_1111;
+		#(cyc) data = 8'b0000_0000;
 		#(cyc) Ivalid = 0;
 		
+		//contiuous zero
+		#(cyc) data=8'b0000_0000; Ivalid = 1;
+		#(cyc*4) Ivalid = 0; 
+		
+		//discontiuous zero
+		#(cyc) data = 8'b0000_0000;Ivalid = 1;
+		#(cyc) Ivalid = 0;
+		#(cyc) Ivalid = 1;
+		#(cyc) Ivalid = 0;
+		#(cyc) Ivalid = 1;
+		#(cyc) Ivalid = 0;
+		#(cyc) Ivalid = 1;
+		#(cyc) Ivalid = 0;
 		
 		
 		#100;

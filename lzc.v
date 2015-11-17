504b626a4bb3fbd6582859e13dba97c888bf7375
jordan_kuo <s104062638@m104.nthu.edu.tw>
2015-11-17 15:31:58 +0800

    Both Turbo & Normal works.

--- lzc.v
+++ lzc.v
@@ -13,12 +13,17 @@ module lzc(
 	output reg [$clog2(width*word) : 0] zeros;
 	output reg Ovalid;
 	
-	reg [$clog2(width*word) : 0]  zeros_next,i;
+	reg [$clog2(width*word) : 0] zeros_next;
+	reg [$clog2(width*word) : 0] i;
 	reg [1:0] state, state_next;
 	reg reg_mode;
+	reg finish;
+	reg finish_next;
 	reg [width-1 : 0] data_next;
-	reg [$clog2(word) : 0] counter_in, inc_in;
-	reg [$clog2(width):0] counter_d;
+	reg [$clog2(word) : 0] counter_in;
+	reg [$clog2(word) : 0] counter_tmp;
+	reg [$clog2(word) : 0] inc_in;
+	reg [$clog2(width*word)-1:0] counter_d;
 	reg [width*word-1 : 0] sum;
 	
 	//main
@@ -27,6 +32,7 @@ module lzc(
 			Ovalid <= 0;
 			zeros <= 0;
 			counter_in <= 0;
+			finish <= 0;
 			state <= IDLE;
 			data_next <= 0;
 		end else begin
@@ -34,6 +40,13 @@ module lzc(
 			reg_mode <= mode;
 			counter_in <= inc_in;
 			data_next <= data;
+			finish <= finish_next;
+		end
+		
+		if(!Ovalid && finish_next)begin
+			counter_tmp = counter_in;
+		end else begin
+			counter_tmp = 0;
 		end
 	end
 	
@@ -49,13 +62,30 @@ module lzc(
 					inc_in = counter_in;
 				end
 			end else begin//TURBO
-				
+				if(counter_in == word-1) begin
+					inc_in = 0;
+					finish_next = 1;
+				end else begin
+					if(data_next == 0)begin
+						inc_in = counter_in + 1;
+						finish_next = 0;
+					end else begin
+						inc_in = 0;
+						finish_next = 1;
+					end
+				end
 			end
 		end else begin
 			if(counter_in == word)begin
 				inc_in = 0;
+				finish_next = 0;
 			end else begin
 				inc_in = counter_in;
+				if(counter_in == 0)begin
+					finish_next = 0;
+				end else begin
+					finish_next = finish;
+				end
 			end
 		end
 	end
@@ -97,12 +127,36 @@ module lzc(
 			end
 			
 			TURBO: begin
-				if(Ovalid)begin
-					zeros = zeros_next;
+				if(finish && counter_in != word)begin
+					Ovalid = 1;
+						for(counter_d=0;counter_d<width;counter_d=counter_d+1)begin
+							if(data_next[counter_d])begin
+								zeros_next = 8*counter_tmp + width-1-counter_d;
+							end else begin
+								zeros = zeros_next;
+							end
+						end
 					state_next = IDLE;
 				end else begin
-					state_next = state;
+					for(counter_d=0;counter_d<width;counter_d=counter_d+1)begin
+						if(data_next[counter_d])begin
+							zeros_next = 8*counter_tmp + width-1-counter_d;
+						end else begin
+							zeros = zeros_next;
+						end
+					end
+					
+					if(counter_in == word-1)begin
+						zeros_next = width*word;
+					end else begin
+						zeros_next = zeros;
+					end
 				end
+			end
+			
+			default: begin
+				zeros_next = 0;
+				state_next = IDLE;
 			end
 		endcase
 	end
--- lzc_t.v
+++ lzc_t.v
@@ -23,15 +23,20 @@ module stimulus;
 	end
 	
 	initial begin
-		clk = 1;
-		#(cyc) rst_n = 0;
+		clk = 1;Ivalid = 0;mode = 0;data=0;rst_n = 0;
 		#(cyc) rst_n = 1;
-		#(cyc) mode = 0;
+		#(cyc) mode = 1;
+		
+		#(cyc) Ivalid = 1;
+		#(cyc) data = 8'b0011_1111;
+		#(cyc) data = 8'b0000_0000;
+		#(cyc) data = 8'b0000_1111;
+		#(cyc) Ivalid = 0;
 		
 		//contiuous input
 		#(cyc) data = 8'b0000_0001;
 		#(cyc) Ivalid = 1;
-		#(cyc*4) Ivalid = 0;
+		#(cyc*4) data = 8'b0000_1111;Ivalid = 0;
 		
 		//discontiuous input
 		#(cyc) data = 8'b0011_1111;Ivalid = 1;
@@ -64,6 +69,50 @@ module stimulus;
 		#(cyc) Ivalid = 1;
 		#(cyc) Ivalid = 0;
 		
+		//---------------NORMAL--------------------
+		#(cyc) mode = 0;
+		
+		#(cyc) Ivalid = 1;
+		#(cyc) data = 8'b0011_1111;
+		#(cyc) data = 8'b0000_0000;
+		#(cyc) data = 8'b0000_1111;
+		#(cyc) Ivalid = 0;
+		
+		//contiuous input
+		#(cyc) data = 8'b0000_0001;
+		#(cyc) Ivalid = 1;
+		#(cyc*4) data = 8'b0000_1111;Ivalid = 0;
+		
+		//discontiuous input
+		#(cyc) data = 8'b0011_1111;Ivalid = 1;
+		#(cyc) Ivalid = 0;
+		#(cyc) Ivalid = 1;
+		#(cyc) Ivalid = 0;
+		#(cyc) Ivalid = 1;
+		#(cyc) Ivalid = 0;
+		#(cyc) data = 8'b0000_0000; Ivalid = 1; 
+		#(cyc) Ivalid = 0;
+		
+		//contiuous diff. input
+		#(cyc) data = 8'b0001_0000;Ivalid = 1;
+		#(cyc) data = 8'b0011_1111;
+		#(cyc) data = 8'b1111_1111;
+		#(cyc) data = 8'b0000_0000;
+		#(cyc) Ivalid = 0;
+		
+		//contiuous zero
+		#(cyc) data=8'b0000_0000; Ivalid = 1;
+		#(cyc*4) Ivalid = 0; 
+		
+		//discontiuous zero
+		#(cyc) data = 8'b0000_0000;Ivalid = 1;
+		#(cyc) Ivalid = 0;
+		#(cyc) Ivalid = 1;
+		#(cyc) Ivalid = 0;
+		#(cyc) Ivalid = 1;
+		#(cyc) Ivalid = 0;
+		#(cyc) Ivalid = 1;
+		#(cyc) Ivalid = 0;
 		
 		#100;
 		$finish;
